1174d84649851bca94330b4af5d2cb67a6785e95
kmg0695 <79101566+kmg0695@users.noreply.github.com>
2025-03-19 19:33:35 -0400

    Test

--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ template_path = root / "template.html"
 
 def main():
     try:
+        print("Testing...")
         print("Deleting public folder....")
         if public.exists():
             shutil.rmtree(public)
